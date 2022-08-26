80a7957769cdd871b59bde3fac6c10c8d6fc56bf
RyanLin11 <ryanlin118@gmail.com>
2022-08-26 09:20:30 -0400

    Add Redux

---
+++ src/services/BaseQuery.js
@@ -0,0 +1,21 @@
+// Source: https://redux-toolkit.js.org/rtk-query/usage/customizing-queries#customizing-query-responses-with-transformresponse
+
+import axios from 'axios';
+
+const axiosBaseQuery = ({ baseUrl } = { baseUrl: '' }) =>
+    async ({ url, method, data, params }) => {
+        try {
+            const result = await axios({ url: baseUrl + url, method, data, params});
+            return { data: result.data }
+        } catch (axiosError) {
+            let err = axiosError;
+            return {
+                error: {
+                    status: err.response?.status,
+                    data: err.response?.data || err.message,
+                }
+            }
+        }
+    }
+
+export default axiosBaseQuery;
